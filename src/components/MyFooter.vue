a41784106f01a5d13edff49d9094ab98677de38b
Poted <o.jpkmc@gmail.com>
2025-01-04 18:00:21 +0100

    refactor: :art: remove unused files and modal from footer

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -1,39 +1,14 @@
 <template>
   <footer>
     <p>&copy; 2024 FizjoStacja. Wszelkie prawa zastrzeżone.</p>
-
-    
-
-    <!-- <div class="pote">
-      <button @click="openModal">
-        <h1>
-          potrzebujesz strony?
-        </h1>
-      </button>
-      <div id="modal">
-        <Modal ref="modalRef" />
-      </div>
-    </div> -->
   </footer>
 </template>
   
 <script>
 
-import Modal from "./PoteModal.vue";
-
 export default {
-  components: {
-    Modal
-  },
+  components: {},
   methods: {
-    openModal() {
-      this.$refs.modalRef.openModal();
-
-      setTimeout(() => {
-        this.goTop();
-      }, 50);
-
-    },
     goTop() {
       const section = document.getElementById('modal');
       section.scrollIntoView({ behavior: 'smooth' });
@@ -44,31 +19,7 @@ export default {
 </script>
   
 <style lang="scss" scoped>
-@keyframes color {
-  0% {
-    border-color: #33CCCC;
-  }
 
-  20% {
-    border-color: #33CC36;
-  }
-
-  40% {
-    border-color: #B8CC33;
-  }
-
-  60% {
-    border-color: #FCCA00;
-  }
-
-  80% {
-    border-color: #33CC36;
-  }
-
-  100% {
-    border-color: #33CCCC;
-  }
-}
 
 footer {
   background-color: #333;
@@ -84,27 +35,6 @@ footer {
     font-size: small;
   }
 
-  .pote {
-
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
-    button {
-      background: none;
-      border-radius: 15px;
-      border-color: greenyellow;
-      padding: 0 12px 0 12px;
-
-      animation: color 5s infinite linear;
-
-    }
-
-
-    button:hover {
-      cursor: pointer;
-    }
-  }
 }
 </style>
   
